b0bfa4fad6fd49e424ce48e2c09939c7123de874
yhswjtuilmare <yh_swjtu@163.com>
2018-03-20 17:33:49 +0800

    update at March 20th

--- mysql_source/run.sh
+++ mysql_source/run.sh
@@ -1,9 +1,11 @@
 pv=${PWD}/mysql_pv.yaml
 pvc=${PWD}/mysql_pvc.yaml
 rc=${PWD}/mysql_rc.yaml
+svc=${PWD}/mysql_svc.yaml
 param_pv=${1}
 param_pvc=${2}
 param_rc=${3}
+param_svc=${4}
 #----------------------------------------------------------
 #运行run.sh时传递三个参数。第一个参数说明pv的名称，第二个参数
 #说明pvc的名称，第三个参数说明rc的名称。若不传递则三个变量自动
@@ -17,6 +19,9 @@ if [ -z ${param_pvc} ]; then
 fi
 if [ -z ${param_rc} ]; then
   param_rc="mysql"
+fi
+if [ -z ${param_svc} ]; then
+  param_svc="mysql"
 fi
 #----------------------------------------------------------
 #以下自动检测kubernetes中是否存在相应的pv，pvc和rc，若不存在则主动创建
@@ -35,7 +40,7 @@ if [ -e ${pv} -a -e ${pvc} -a -e ${rc} ]; then
   if [ ${#var[0]} == 0 ]; then
     echo There is no PV like \"${param_pv}\", need to create a new PV;
     kubectl create -f ${pv}
-    echo A new PV created
+    echo A new PV created;
   else
     echo There is already a PV, no need to create a new one;
     echo ${var[0]}
@@ -44,7 +49,7 @@ if [ -e ${pv} -a -e ${pvc} -a -e ${rc} ]; then
   if [ ${#var[0]} == 0 ]; then
     echo There is no PVC like \"${param_pvc}\", need to create a new one;
     kubectl create -f ${pvc}
-    echo A new PVC created
+    echo A new PVC created;
   else
     echo There is already a PV, no need to create a new one;
     echo ${var[0]}
@@ -53,13 +58,22 @@ if [ -e ${pv} -a -e ${pvc} -a -e ${rc} ]; then
   if [ ${#var[0]} == 0 ]; then
     echo There is no ReplicationController like \"${param_rc}\", need to create new one;
     kubectl create -f ${rc}
-    echo A new ReplicationController created
+    echo A new ReplicationController created;
   else
-    echo There is already a ReplicationController, no need to create a new one
+    echo There is already a ReplicationController, no need to create a new one;
+    echo ${var[0]}
+  fi
+  var=`kubectl get svc | grep ${param_svc}`
+  if [ ${#var[0]} == 0 ]; then
+    echo There is no Service like \"${param_svc}\", need to create new one;
+    kubectl create -f ${svc}
+    echo A new Service created;
+  else
+    echo There is already a Service , no need to create a new one;
     echo ${var[0]}
   fi
   unset var
 else
   echo Can not find enough yaml files
 fi
-unset pv pvc rc param_pv param_pvc param_rc
+unset pv pvc rc svc param_pv param_pvc param_rc param_svc
