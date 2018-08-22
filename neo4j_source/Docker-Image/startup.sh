a6e7994cd9e1e3d4c6cf1623e9173eb6c456dd4b
yhswjtuilmare <yh_swjtu@163.com>
2018-08-22 10:18:19 +0800

    update at Aug 22th

--- neo4j_source/Docker-Image/startup.sh
+++ neo4j_source/Docker-Image/startup.sh
@@ -4,6 +4,15 @@
 
 sleep 20
 
+echo "dbms.jvm.additional=-Dcom.sun.management.jmxremote.port=3637" >> /var/lib/neo4j/conf/neo4j.conf
+echo "dbms.jvm.additional=-Dcom.sun.management.jmxremote.authenticate=false" >> /var/lib/neo4j/conf/neo4j.conf
+echo "dbms.jvm.additional=-Dcom.sun.management.jmxremote.ssl=false" >> /var/lib/neo4j/conf/neo4j.conf
+echo "dbms.jvm.additional=-Dcom.sun.management.jmxremote.password.file=/absolute/path/to/conf/jmx.password" >> /var/lib/neo4j/conf/neo4j.conf
+echo "dbms.jvm.additional=-Dcom.sun.management.jmxremote.access.file=/absolute/path/to/conf/jmx.access" >> /var/lib/neo4j/conf/neo4j.conf
+echo "dbms.jvm.additional=-Djava.rmi.server.hostname=127.0.0.1" >> /var/lib/neo4j/conf/neo4j.conf
+
+cd / && java -jar neo4jAgent-0.0.1-jar-with-dependencies.jar &
+
 if [ ${#PEER_DISCOVERY_SERVICE} == 0 ]; then
   echo `date` "[Error] - "\"PEER_DISCOVERY_SERVICE \" "was not found."
 else
